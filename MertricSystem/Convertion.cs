a2423278198159c73c9c142f1e5eb42ae1d1c0b0
SunTea07 <martingabrielflores@yahoo.com>
2015-12-14 21:20:20 +0800

    Gram to Pound Converter and Pricer
    The program will ask for a number of gram then it will convert it to
    pound i have initialized a product called Montreal Smoked Meat which is
    $2.00 per 100 gram then after the conversion the number of pounds will
    be also priced.

---
+++ MertricSystem/Convertion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MertricSystem
+{
+    class Convertion
+    {
+        static void Main(string[] args)
+        {
+        double pounds;
+        double price_per_gram;
+        Console.WriteLine("Enter number of grams to be converted to pounds: ");
+       
+        double grams = Convert.ToDouble(Console.ReadLine());
+        double Montreal_Smoked_Meat = grams * 0.0209;   
+        
+        pounds = grams * 0.00220462;
+        price_per_gram = pounds * 453.592;
+        
+        Console.WriteLine(" Grams in pounds is : " + pounds);
+        Console.WriteLine(" The Price of the product in pounds is : " + Montreal_Smoked_Meat.ToString("C2"));
+        //Console.WriteLine(" The Price of your product in pound is:" + price_per_gram.ToString("C6"));
+        Console.ReadKey();  
+        
+        }
+    }
+}
+
+
